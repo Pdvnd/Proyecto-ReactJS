1d09e854980bb468da39b7bd11c09097bf54599d
Paola Nuñez <98001281+Pdvnd@users.noreply.github.com>
2022-08-17 22:33:08 -0300

    Delete src/src directory

--- src/src/components/Titulo/Titulo.js
+++
@@ -1,7 +0,0 @@
-import './Titulo.css';
-export const Titulo= () => {
-    return(
-    <div>
-    <img className="cover" src='https://i.postimg.cc/Tww0thgK/Estudio-de-Fotograf-a-Portada-de-Facebook.png'></img>
-    </div>
-)};
