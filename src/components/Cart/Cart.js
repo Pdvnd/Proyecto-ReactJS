b17079aedc16df40530b44b61b7297cefc7f1f37
Paola Nuñez <98001281+Pdvnd@users.noreply.github.com>
2022-08-29 13:34:44 -0300

    Item Collection II

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,29 @@
-import React from "react"
+import React from "react";
+import {addDoc, collection, getFirestore} from "firebase/firestore";
 import { useCartContext } from "../../CartContext/CartContext"
 import { Link } from "react-router-dom";
 import { ItemCart } from "../ItemCart/ItemCart";
 
 export const Cart = () => {
     const {cart, totalPrecio} = useCartContext();
+
+    const order = {
+        buyer: {
+            name: 'Paola',
+            email: 'paolanu@gmail.com',
+            phone: '3517658746',
+            address: 'lomito'
+        },
+        items: cart.map(product => ({id: product.id, tittle: product.title, price: product.price, quantity: product.quantity})),
+        total: totalPrecio(),
+    }
+    const handleClick = () => {
+        const db = getFirestore();
+        const ordersCollection = collection(db, 'orders');
+        addDoc(ordersCollection, order)
+        .then(({id}) => console.log(id));
+    }
+    
 
     if (cart.length === 0){
     return (
@@ -21,8 +40,9 @@ return (
         cart.map(product => <ItemCart key={product.id} product={product}/>)
     }
     <p>
-        total: {totalPrecio()};
+        Total a Pagar: {totalPrecio()}
     </p>
+    <button onClick={handleClick}>Emitir Compra</button>
     </>
 )
 }
